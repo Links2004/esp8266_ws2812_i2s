ed2121759f83388b02f87eb8e33cea68001be803
Scott Lawson <scottlawsonbc@gmail.com>
2017-01-07 15:41:45 -0800

    Added more detail to comments, note about gamma correction bug
    Added more detailed comments for the options in `ws2812.h`. Added note
    indicating that you cannot disable gamma correction (related to
    JoDaNl/esp8266_ws2812_i2s#9).

--- include/ws2812.h
+++ include/ws2812.h
@@ -3,11 +3,22 @@
 #ifndef __WS2812_H__
 #define __WS2812_H__
 
-#define WS2812_GAMMA_CORRECTION (1)         // 0 = off, 1=on (I might change this in the near
-                                            // future to support different gamma curves) 
-#define WS2812_DITHER_NUM       (8)         // 1, 2, 4, 8  (1 = no dithering)
-#define WS2812_USE_INTERRUPT    (0)         // not supported yet
+// Temporal Dithering
+// Dithering preserves color and light when brightness is low.
+// If you notice flickering, try reducing the amount of dithering.
+// 1 = Disable temporal dithering
+// 2, 6, 8 = Enable temporal dithering (larger values = more dithering)
+#define WS2812_DITHER_NUM (8)
 
+// Gamma correction
+// Uses lookup table to correct for human non-linear brightness perception.
+// 1 = enable gamma correction
+// 0 = disable gamma correction
+// NOTE: Disabling gamma correction is not yet suported. This is always enabled.
+#define WS2812_GAMMA_CORRECTION (1) // Disabling is not supporting yet
+
+// This option is not supported yet
+#define WS2812_USE_INTERRUPT (0)
 
 #endif
 
